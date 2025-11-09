e5a97038501f2f92960f18c71fdf0a5570379cef
BlueGeckoJP <kei09doya@gmail.com>
2025-11-09 15:39:56 +0900

    feat: update main function to read input from stdin and add JSONC test file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::io::{self, Read};
+
 mod lexer;
 mod token;
 
-fn main() {
-    let mut lexer = lexer::Lexer::new("{ \"key\": 123 }".to_string());
+fn main() -> io::Result<()> {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf)?;
+
+    let mut lexer = lexer::Lexer::new(buf);
     lexer.tokenize();
     for token in lexer.tokens {
         println!("{:?}", token);
     }
+
+    Ok(())
 }
